148a2c0216d1c4f7c6ac61fb2055018baa437bd1
Luis Eduardo Cochachi Chamorro <109692666+cochachyLE-Dev@users.noreply.github.com>
2022-09-02 20:28:08 -0500

    .

---
+++ src/main/java/pe/com/bootcamp/KafkaServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaServiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaServiceManagerApplication.class, args);
+	}
+
+}
